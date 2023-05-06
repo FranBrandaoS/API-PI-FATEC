a4e67ac1ceba199039a1062379fca51bcfd83730
Nicolas <nicobarbosa20034@gmail.com>
2023-05-05 22:30:29 -0300

    adicionando cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import express from 'express'
 import db from './db.js'
 import funcionariosRoutes from './routes/funcionariosRoutes.js'
 import clientesRoutes from './routes/clienteRoutes.js'
+import cors from 'cors'
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
 
 db.sync(() => console.log('Banco de dados preparado'))
 app.use('/funcionarios', funcionariosRoutes)
