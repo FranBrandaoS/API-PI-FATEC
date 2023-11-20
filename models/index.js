cdd9445ba74eab99c136e8c981b0a32c431859a2
Nicolas Barbosa <nicobarbosa20034@gmail.com>
2023-11-20 17:10:43 -0300

    arrumando tabela tokens

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 import Clientes from "./clienteModel.js";
 import Tokens from "./tokenModel.js";
-import db from "../db.js";
 
 Clientes.hasOne(Tokens, {foreignKey: 'userId'})
 Tokens.belongsTo(Clientes, {foreignKey: 'userId'})
