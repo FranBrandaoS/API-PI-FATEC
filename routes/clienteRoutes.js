d8088351836880b6be6c59cdf2c56b082d3ee48a
Nicolas Barbosa <nicobarbosa20034@gmail.com>
2023-11-21 00:09:14 -0300

    voltando com estava

--- routes/clienteRoutes.js
+++ routes/clienteRoutes.js
@@ -5,9 +5,9 @@ const cliente_router = express.Router()
 
 cliente_router.get('/', clienteController.getAllCliente)
 cliente_router.post('/', clienteController.createCliente)
-cliente_router.delete('/id',clienteController.deleteCliente)
-cliente_router.put('/id',clienteController.updateCliente)
-cliente_router.get('/id',clienteController.getClienteById)
+cliente_router.delete('/:id',clienteController.deleteCliente)
+cliente_router.put('/:id',clienteController.updateCliente)
+cliente_router.get('/:id',clienteController.getClienteById)
 cliente_router.post('/login', clienteController.loginCliente)
 
 export default cliente_router
